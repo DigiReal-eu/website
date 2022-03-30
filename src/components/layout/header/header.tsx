620c8517d463d437e0d95086d818c0a71ac1e4fa
Kalle Pronk <kallepronk@gmail.com>
2022-03-30 09:15:50 +0200

    Changed header to new design

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './header.scss';
 import '../../../App.scss';
-import HeaderLogo from '../../../assets/images/digireal_logo_small.png';
+import HeaderLogo from '../../../assets/images/digireal_logo_full.png';
 
 function Header() {
     return (
